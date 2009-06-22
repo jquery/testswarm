316ae157d10bd661b5aa09bf2b666cf344520e1f
John Resig <jeresig@gmail.com>
2009-06-22 11:54:41 -0700

    Added an automated process for wiping dead client runs, freeing them up for other clients to take over.

---
+++ logic/wipe.php
@@ -0,0 +1,17 @@
+<?php
+
+	$result = mysql_queryf("SELECT run_id, client_id, useragent_id FROM run_client, clients WHERE DATE_ADD(run_client.updated, INTERVAL 5 MINUTE) < NOW() AND clients.id = client_id;");
+
+	while ( $row = mysql_fetch_array($result) ) {
+		$run_id = $row[0];
+		$client_id = $row[1];
+		$useragent_id = $row[2];
+
+		# Update run_useragent (clients, useragents)
+		mysql_queryf("UPDATE run_useragent SET runs = runs - 1 WHERE run_id=%u AND useragent_id=%u;", $run_id, $useragent_id);
+		mysql_queryf("DELETE FROM run_client WHERE run_id=%u AND client_id=%u;", $run_id, $client_id);
+	}
+
+	echo "done";
+	exit;
+?>
