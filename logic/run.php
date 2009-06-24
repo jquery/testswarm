744474bd99183d2991553eed3463f045fc318db4
John Resig <jeresig@gmail.com>
2009-06-24 11:36:54 -0700

    Fixed an issue with clients being double-reported.

--- logic/run.php
+++ logic/run.php
@@ -1,15 +1,15 @@
 <?php
-	include "inc/init.php";
-
-	$title = "Run the Test Swarm";
-
-	$scripts = "";
-
 	if ( $_REQUEST['user'] ) {
+		$_SESSION['username'] = ereg_replace("[^a-zA-Z0-9_ -]", "", $_REQUEST['user']);
 		session_write_close();
 		header("Location: /run/");
 		exit;
 	}
+
+	include "inc/init.php";
+
+	$title = "Run the Test Swarm";
+	$scripts = "";
 
 	if ( $client_id ) {
 		$scripts = "<script type='text/javascript'>var client_id = $client_id;</script>";
