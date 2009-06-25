7d3e961c5b69fd928dd32b01f3250619bdc78d8d
John Resig <jeresig@gmail.com>
2009-06-25 11:43:41 -0700

    Added some more detailed commands to the reset SQL.

--- reset.sql
+++ reset.sql
@@ -1,6 +1,13 @@
+-- Resets the entire database
 TRUNCATE clients;
 TRUNCATE jobs;
 TRUNCATE run_client;
 TRUNCATE run_useragent;
 TRUNCATE runs;
-TRUNCATE users;
+TRUNCATE users; -- Optional
+
+-- Resets just the results from clients
+UPDATE jobs SET `status`=0 WHERE 1=1;
+UPDATE run_useragent SET `runs`=0, `completed`=0, `status`=0 WHERE 1=1;
+UPDATE runs SET `status`=0 WHERE 1=1;
+DELETE FROM run_client WHERE 1=1;
