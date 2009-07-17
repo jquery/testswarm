0582b7436011ee6341693cf21e70b503fb643428
John Resig <jeresig@gmail.com>
2009-07-17 08:00:41 -0700

    Limited the insertion of job status HTML to only when something changed.

--- js/status.js
+++ js/status.js
@@ -1,14 +1,19 @@
 jQuery(function(){
 	setTimeout(function(){
 		if ( jQuery("td.notdone").length ) {
-			jQuery("table.results").load(window.location.href + " tbody");
+			jQuery.get(window.location.href, function(html){
+				var table = jQuery("table", html).html(), insert = jQuery("table.results");
+				if ( table !== insert.html() ) {
+					insert.html( table );
+				}
+			});
 			setTimeout(arguments.callee, 5000);
 		}
 	}, 5000);
 	
 	jQuery("td:has(a)").live("dblclick", function(){
 		var params = /&.*$/.exec( jQuery(this).find("a").attr("href") );
-		$.ajax({
+		jQuery.ajax(
 			url: "/",
 			type: "POST",
 			data: "state=wiperun" + params
