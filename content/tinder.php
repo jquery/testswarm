689e37749d3f744a241cc856e59a3ee44f27355e
John Resig <jeresig@gmail.com>
2009-07-19 13:48:29 -0700

    Limit the number of shown jobs in the tinderbox (need to give it pagination at some point).

--- content/tinder.php
+++ content/tinder.php
@@ -58,7 +58,7 @@
 	$job_search = ereg_replace("[^a-zA-Z ]", "", $_REQUEST['job']);
 	$job_search .= "%";
 
-	$search_result = mysql_queryf("SELECT jobs.name, jobs.status, jobs.id FROM jobs, users WHERE jobs.name LIKE %s AND users.name=%s AND jobs.user_id=users.id ORDER BY name DESC;", $job_search, $search_user);
+	$search_result = mysql_queryf("SELECT jobs.name, jobs.status, jobs.id FROM jobs, users WHERE jobs.name LIKE %s AND users.name=%s AND jobs.user_id=users.id ORDER BY name DESC LIMIT 15;", $job_search, $search_user);
 
 	echo "<br/><h3>Recent Jobs:</h3><table class='results'><tbody>";
 
