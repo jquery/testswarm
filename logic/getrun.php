4d3a1c054dd398d1aeba07af66dccd7081acca8f
John Resig <jeresig@gmail.com>
2009-07-21 06:24:08 -0700

    Optimized the queries behind getrun (reducing the number of joins, removing the updates to the parent table statuses).

--- logic/getrun.php
+++ logic/getrun.php
@@ -1,38 +1,31 @@
 <?php
 	include "inc/init.php";
 
-	$result = mysql_queryf("SELECT run_id, url FROM run_useragent, runs WHERE runs.id=run_useragent.run_id AND run_useragent.useragent_id=%u AND run_useragent.runs < run_useragent.max ORDER BY run_id;", $useragent_id);
+	$result = mysql_queryf("SELECT run_id FROM run_useragent WHERE useragent_id=%u AND runs < max ORDER BY run_id LIMIT 1;", $useragent_id);
 	
 	# A run was found
 	if ( $row = mysql_fetch_array($result) ) {
 		$run_id = $row[0];
-		$url = $row[1];
+
+		$result = mysql_queryf("SELECT url FROM runs WHERE id=%u LIMIT 1;", $run_id);
+
+		# TODO: Return more run info to the client (Name, etc.)
+		if ( $row = mysql_fetch_array($result) ) {
+			$url = $row[0];
+		}
 	
 		# Make sure that we don't re-run the tests in the same client
-		$result = mysql_queryf("SELECT * FROM run_client WHERE run_id=%u AND client_id=%u;", $run_id, $client_id);
+		$result = mysql_queryf("SELECT 1 FROM run_client WHERE run_id=%u AND client_id=%u LIMIT 1;", $run_id, $client_id);
 
 		if ( mysql_num_rows($result) == 0 ) {
 			# Mark the run as "in progress" on the useragent
-			mysql_queryf("UPDATE run_useragent SET runs = runs + 1, status = 1 WHERE run_id=%u AND useragent_id=%u;", $run_id, $useragent_id);
-
-			# Mark the complete run as "in progress"
-			mysql_queryf("UPDATE runs SET status = 1 WHERE id=%u;", $run_id);
-
-			$result = mysql_queryf("SELECT job_id FROM runs WHERE id=%u;", $run_id);
-
-			if ( $row = mysql_fetch_array($result) ) {
-				# Mark the complete job as "in progress"
-				mysql_queryf("UPDATE jobs SET status = 1 WHERE id=%u;", $row[0]);
-			}
+			mysql_queryf("UPDATE run_useragent SET runs = runs + 1, status = 1 WHERE run_id=%u AND useragent_id=%u LIMIT 1;", $run_id, $useragent_id);
 
 			# Initialize the client run
 			mysql_queryf("INSERT INTO run_client (run_id,client_id,status,created) VALUES(%u,%u,1,NOW());", $run_id, $client_id);
 
 			echo "$run_id $url";
 		}
-
-		# TODO: There needs to be a cronjob that marks dead clients as inactive
-		#       and decrements the run_useragent run count.
 	}
 
 	exit();
