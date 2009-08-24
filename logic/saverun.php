09c92cbd1a32811cbfa0ae73cd7e0146f242b702
John Resig <jeresig@gmail.com>
2009-08-24 07:24:58 -0700

    Made the logic for deleting bad client runs more deliberate (works better now).

--- logic/saverun.php
+++ logic/saverun.php
@@ -14,7 +14,12 @@
 		# If we're 100% passing we don't need any more runs
 		if ( $fail == 0 && $error == 0 ) {
 			# Clear out old runs that were bad, since we now have a good one
-			mysql_queryf("DELETE FROM run_client WHERE run_id=%u AND client_id!=%u AND EXISTS (SELECT useragent_id FROM clients WHERE id=client_id AND useragent_id=%u);", $run_id, $client_id, $useragent_id);
+			$result = mysql_queryf("SELECT client_id FROM run_client, clients WHERE run_id=%u AND client_id!=%u AND client_id=clients.id AND clients.useragent_id=%u;", $run_id, $client_id, $useragent_id);
+
+			while ( $row = mysql_fetch_array($result) ) {
+				mysql_queryf("DELETE FROM run_client WHERE run_id=%u AND client_id=%u;", $run_id, $row[0]);
+			}
+
 			mysql_queryf("UPDATE run_useragent SET runs = max, completed = completed + 1, status = 2 WHERE useragent_id=%u AND run_id=%u LIMIT 1;", $useragent_id, $run_id);
 		} else {
 			mysql_queryf("UPDATE run_useragent SET completed = completed + 1, status = IF(completed+1<max, 1, 2) WHERE useragent_id=%u AND run_id=%u LIMIT 1;", $useragent_id, $run_id);
