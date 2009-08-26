8878ae1321aee68aeb3b9aa114c8b74169268aec
John Resig <jeresig@gmail.com>
2009-08-26 09:55:51 -0700

    Added logic for reseting cases where a race condition blocks a run form occurring.

--- logic/wipe.php
+++ logic/wipe.php
@@ -12,6 +12,9 @@
 		mysql_queryf("DELETE FROM run_client WHERE run_id=%u AND client_id=%u;", $run_id, $client_id);
 	}
 
+	# Reset runs that race-condition deleted themselves
+	mysql_queryf("UPDATE run_useragent SET runs=0, completed=0, status=0 WHERE runs=max AND NOT EXISTS (SELECT * FROM run_client, clients WHERE run_client.run_id=run_useragent.run_id AND run_client.client_id=clients.id AND clients.useragent_id=run_useragent.useragent_id);");
+
 	echo "done";
 	exit;
 ?>
