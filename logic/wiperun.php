1b6f2114689006e37778927fcdb22cdd05b03674
John Resig <jeresig@gmail.com>
2009-08-26 17:50:22 -0700

    Changed the logic of wiperun to wipe all runs from clients of the same useragent and then reset run_useragent (allowing for a full new batch of runs to come in).

--- logic/wiperun.php
+++ logic/wiperun.php
@@ -14,8 +14,8 @@
 			if ( $row = mysql_fetch_row($results) ) {
 				$useragent_id = $row[0];
 
-				mysql_queryf("DELETE FROM run_client WHERE run_id=%u AND client_id=%u;", $run_id, $client_id);
-				mysql_queryf("UPDATE run_useragent SET status=1, runs=runs-1, completed=completed-1, updated=NOW() WHERE run_id=%u AND useragent_id=%u;", $run_id, $useragent_id);
+				mysql_queryf("DELETE run_client FROM run_client,clients WHERE run_id=%u AND clients.id=client_id AND clients.useragent_id=%u;", $run_id, $useragent_id);
+				mysql_queryf("UPDATE run_useragent SET status=0, runs=0, completed=0, updated=NOW() WHERE run_id=%u AND useragent_id=%u;", $run_id, $useragent_id);
 				mysql_queryf("UPDATE runs SET status=1, updated=NOW() WHERE run_id=%u;", $run_id);
 			}
 		}
