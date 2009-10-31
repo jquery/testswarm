98056be2664ccb55d5ad5b34c76a661ca21dccf2
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-10-31 18:11:04 +0800

    allow addjob to work with GET also

--- logic/addjob.php
+++ logic/addjob.php
@@ -2,9 +2,9 @@
 
 	$title = "Add New Job";
 
-	if ( $_POST['state'] == "addjob" ) {
-		$username = preg_replace("/[^a-zA-Z0-9_ -]/", "", $_POST['user']);
-		$auth = preg_replace("/[^a-z0-9]/", "", $_POST['auth']);
+	if ( $_REQUEST['state'] == "addjob" ) {
+		$username = preg_replace("/[^a-zA-Z0-9_ -]/", "", $_REQUEST['user']);
+		$auth = preg_replace("/[^a-z0-9]/", "", $_REQUEST['auth']);
 
 		$result = mysql_queryf("SELECT id FROM users WHERE name=%s AND auth=%s;", $username, $auth);
 
@@ -18,29 +18,29 @@
 		}
 
 		mysql_queryf("INSERT INTO jobs (user_id,name,created) VALUES(%u,%s,NOW());",
-			$user_id, $_POST['job_name']);
+			$user_id, $_REQUEST['job_name']);
 
 		$job_id = mysql_insert_id();
 
-		foreach ( $_POST['suites'] as $suite_num => $suite_name ) {
+		foreach ( $_REQUEST['suites'] as $suite_num => $suite_name ) {
 			if ( $suite_name ) {
-				#echo "$suite_num " . $_POST['suites'][$suite_num] . " " . $_POST['urls'][$suite_num] . "<br>";
+				#echo "$suite_num " . $_REQUEST['suites'][$suite_num] . " " . $_REQUEST['urls'][$suite_num] . "<br>";
 				mysql_queryf("INSERT INTO runs (job_id,name,url,created) VALUES(%u,%s,%s,NOW());",
-					$job_id, $suite_name, $_POST['urls'][$suite_num]);
+					$job_id, $suite_name, $_REQUEST['urls'][$suite_num]);
 
 				$run_id = mysql_insert_id();
 
 				$ua_type = "1 = 1";
 
-				if ( $_POST['browsers'] == "popular" ) {
+				if ( $_REQUEST['browsers'] == "popular" ) {
 					$ua_type = "popular = 1";
-				} else if ( $_POST['browsers'] == "current" ) {
+				} else if ( $_REQUEST['browsers'] == "current" ) {
 					$ua_type = "current = 1";
-				} else if ( $_POST['browsers'] == "gbs" ) {
+				} else if ( $_REQUEST['browsers'] == "gbs" ) {
 					$ua_type = "gbs = 1";
-				} else if ( $_POST['browsers'] == "beta" ) {
+				} else if ( $_REQUEST['browsers'] == "beta" ) {
 					$ua_type = "beta = 1";
-				} else if ( $_POST['browsers'] == "popularbeta" ) {
+				} else if ( $_REQUEST['browsers'] == "popularbeta" ) {
 					$ua_type = "(popular = 1 OR beta = 1)";
 				}
 
@@ -49,14 +49,14 @@
 				while ( $row = mysql_fetch_array($result) ) {
 					$browser_num = $row[0];
 					mysql_queryf("INSERT INTO run_useragent (run_id,useragent_id,max,created) VALUES(%u,%u,%u,NOW());",
-						$run_id, $browser_num, $_POST['max']);
+						$run_id, $browser_num, $_REQUEST['max']);
 				}
 			}
 		}
 
 		$url = "/job/" . $job_id . "/";
 
-		if ( $_POST['output'] == "dump" ) {
+		if ( $_REQUEST['output'] == "dump" ) {
 			echo $url;
 		} else {
 			header("Location: $url");
