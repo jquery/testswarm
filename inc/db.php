7dcc0d7c6e335be94558db3ec375cd20aa738f61
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-10-31 12:39:00 +0800

    db connection error handling so you can catch errors in your configuration quicker

--- inc/db.php
+++ inc/db.php
@@ -5,6 +5,12 @@
 		$config['database']['username'],
 		$config['database']['password']
 	);
+	if (!$db) {
+	    die('Not connected: ' . mysql_error());
+	}
 
-	mysql_select_db($config['database']['database']);
+	$db_selected = mysql_select_db($config['database']['database'], $db);
+	if (!$db_selected) {
+	    die ('Can\'t use ' . $config['database']['database'] . ': ' . mysql_error());
+	}
 ?>
