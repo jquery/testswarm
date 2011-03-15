e934e016b3d04e0cb86e5f34c71ae521a3acca96
Peter Parente <parente@cs.unc.edu>
2011-03-14 22:54:09 -0400

    Fix useragent string for FF4 RC

--- config/useragents.sql
+++ config/useragents.sql
@@ -6,7 +6,7 @@ INSERT INTO `useragents` (`name`, `engine`, `version`, `active`, `current`, `pop
 
 ('Firefox 3.5', 'gecko', '^1.9.1[0-9.]*$', 1, 0, 0, 1, 0, 0),
 ('Firefox 3.6', 'gecko', '^1.9.2[0-9.]*$', 1, 0, 1, 1, 0, 0),
-('Firefox 4.0', 'gecko', '^2.0.', 1, 1, 1, 1, 0, 0),
+('Firefox 4.0', 'gecko', '^2.0.?', 1, 1, 1, 1, 0, 0),
 
 ('Internet Explorer 6', 'msie', '^6.', 1, 0, 1, 1, 0, 0),
 ('Internet Explorer 7', 'msie', '^7.', 1, 0, 1, 1, 0, 0),
