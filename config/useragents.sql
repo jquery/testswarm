7265792a68f019170da07f8fd2a39f5564bda8d4
James Coglan <jcoglan@gmail.com>
2011-07-10 11:33:28 -0700

    Add Opera 11.5x to useragents.sql.

--- config/useragents.sql
+++ config/useragents.sql
@@ -17,6 +17,7 @@ INSERT INTO `useragents` (`name`, `engine`, `version`, `active`, `current`, `pop
 ('Opera 10.6x', 'presto', '^2.6.', 1, 0, 1, 0, 0, 0),
 ('Opera 11.0x', 'presto', '^2.7.', 1, 1, 1, 1, 0, 0),
 ('Opera 11.1x', 'presto', '^2.8.', 1, 1, 1, 1, 0, 0),
+('Opera 11.5x', 'presto', '^2.9.', 1, 1, 1, 1, 0, 0),
 
 ('Safari 4.0', 'webkit', '^531.', 1, 0, 1, 1, 0, 0),
 ('Safari 5.0', 'webkit', '^533.', 1, 1, 1, 1, 0, 0),
