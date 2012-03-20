jquery/testswarm

commit 65404b6d4f14ecfe5ca99a4192614ae1eca17b76

Fix no icons rendering in jobstatus and on user page. This fixes Issue #13. It's based on a patch from parente (Peter Parente).

content/tinder.php
@@ -24,7 +24,7 @@
 		}
 	}
 
-	$result = mysql_query("SELECT useragents.engine as engine, useragents.name as name, clients.os as os, DATE_FORMAT(clients.created, '%Y-%m-%dT%H:%i:%sZ') as since FROM users, clients, useragents WHERE clients.useragent_id=useragents.id AND DATE_ADD(clients.updated, INTERVAL 1 minute) > NOW() AND clients.user_id=users.id AND users.name='$search_user' ORDER BY useragents.engine, useragents.name;");
+	$result = mysql_queryf("SELECT useragents.engine as engine, useragents.name as name, clients.os as os, DATE_FORMAT(clients.created, '%Y-%m-%dT%H:%i:%sZ') as since FROM users, clients, useragents WHERE clients.useragent_id=useragents.id AND DATE_ADD(clients.updated, INTERVAL 1 minute) > NOW() AND clients.user_id=users.id AND users.name=%s ORDER BY useragents.engine, useragents.name;", $search_user);
 
 	if ( mysql_num_rows($result) > 0 ) {
 
@@ -77,6 +77,7 @@
 	$output = "";
 	$browsers = array();
 	$addBrowser = true;
+	$last = "";
 
 	while ( $row = mysql_fetch_array($search_result) ) {
 		$job_name = $row[0];
@@ -90,31 +91,9 @@
 
 	$result = mysql_queryf("SELECT runs.id as run_id, runs.url as run_url, runs.name as run_name, useragents.engine as browser, useragents.name as browsername, useragents.id as useragent_id, run_useragent.status as status FROM run_useragent, runs, useragents WHERE runs.job_id=%u AND run_useragent.run_id=runs.id AND run_useragent.useragent_id=useragents.id ORDER BY run_id, browsername;", $job_id);
 
-	$last = "";
-
 	while ( $row = mysql_fetch_assoc($result) ) {
 		if ( $row["run_id"] != $last ) {
 			if ( $last ) {
-				if ( $addBrowser ) {
-					$header = "<tr><th></th>\n";
-					$last_browser = array();
-					foreach ( $browsers as $browser ) {
-						if ( $last_browser["id"] != $browser["id"] ) {
-							$header .= '<th><div class="browser">' .
-								'<img src="' . swarmpath( 'images/' ) . $browser["engine"] .
-								'.sm.png" class="browser-icon ' . $browser["engine"] .
-								'" alt="' . $browser["name"] .
-								'" title="' . $browser["name"] .
-								'"/><span class="browser-name">' .
-								preg_replace('/\w+ /', "", $browser["name"]) . ', ' .
-								'</span></div></th>';
-						}
-						$last_browser = $browser;
-					}
-					$header .= "</tr>\n";
-					$output = $header . $output;
-				}
-
 				$addBrowser = false;
 			}
 
@@ -175,12 +154,33 @@
 		$last = $row["run_id"];
 	}
 
+
 	foreach ( $results as $key => $fail ) {
 		$output .= "<td class='" . $states[$key] . "'></td>";
 	}
 
 	$output .= "</tr>\n";
 
+	}
+	
+	if ( $last ) {
+		$header = "<tr><th></th>\n";
+		$last_browser = array();
+		foreach ( $browsers as $browser ) {
+			if ( $last_browser["id"] != $browser["id"] ) {
+				$header .= '<th><div class="browser">' .
+					'<img src="' . swarmpath( 'images/' ) . $browser["engine"] .
+					'.sm.png" class="browser-icon ' . $browser["engine"] .
+					'" alt="' . $browser["name"] .
+					'" title="' . $browser["name"] .
+					'"/><span class="browser-name">' .
+					preg_replace('/\w+ /', "", $browser["name"]) . ', ' .
+					'</span></div></th>';
+			}
+			$last_browser = $browser;
+		}
+		$header .= "</tr>\n";
+		$output = $header . $output;
 	}
 
 	echo "$output</tr>\n</tbody>\n</table>";

content/jobstatus.php
@@ -60,30 +60,8 @@
 	while ( $row = mysql_fetch_assoc($result) ) {
 		if ( $row["run_id"] != $last ) {
 			if ( $last ) {
-				if ( $addBrowser ) {
-					$header = "<tr><th></th>\n";
-					$last_browser = array();
-					foreach ( $browsers as $browser ) {
-						if ( $last_browser["id"] != $browser["id"] ) {
-							$header .= '<th><div class="browser">' .
-								'<img src="' . swarmpath( 'images/' ) . $browser["engine"] .
-								'.sm.png" class="browser-icon ' . $browser["engine"] .
-								'" alt="' . $browser["name"] .
-								'" title="' . $browser["name"] .
-								'"/><span class="browser-name">' .
-								preg_replace('/\w+ /', "", $browser["name"]) . ', ' .
-								'</span></div></th>';
-						}
-						$last_browser = $browser;
-					}
-					$header .= "</tr>\n";
-					$output = $header . $output;
-				}
-
-				$output .= "</tr>\n";
 				$addBrowser = false;
 			}
-
 			$useragents = array();
 
 			$runResult = mysql_queryf("SELECT run_client.client_id as client_id, run_client.status as status, run_client.fail as fail, run_client.error as error, run_client.total as total, clients.useragent_id as useragent_id FROM run_client, clients WHERE run_client.run_id=%u AND run_client.client_id=clients.id ORDER BY useragent_id;", $row["run_id"]);
@@ -135,6 +113,27 @@
 		#echo "</ul></li>";
 
 		$last = $row["run_id"];
+	}
+
+	if ( $last ) {
+		$header = "<tr><th></th>\n";
+		$last_browser = array();
+		foreach ( $browsers as $browser ) {
+			if ( $last_browser["id"] != $browser["id"] ) {
+				$header .= '<th><div class="browser">' .
+					'<img src="' . swarmpath( 'images/' ) . $browser["engine"] .
+					'.sm.png" class="browser-icon ' . $browser["engine"] .
+					'" alt="' . $browser["name"] .
+					'" title="' . $browser["name"] .
+					'"/><span class="browser-name">' .
+					preg_replace('/\w+ /', "", $browser["name"]) . ', ' .
+					'</span></div></th>';
+			}
+			$last_browser = $browser;
+		}
+		$header .= "</tr>\n";
+		$output = $header . $output;
+		$output .= "</tr>\n";
 	}
 
 	echo "$output</tr>\n</tbody>\n</table>";

commit 60ee4b35defa660869aaf24f82d05bffbbe32c05

Fix tinder bugs - Issue #92 Tinder should show current browsers (regression)   Caused by 65404b6d4f14ecfe5ca99a4192614ae1eca17b76, which moved   $search_user as mysql_queryf argument, which never worked because   DATE_FORMAT already has a "%s" in it.   Fixed by removing the %s from DATE_FORMAT, we don't need it.
- Indention

content/tinder.php
@@ -24,44 +24,60 @@
 		}
 	}
 
-	$result = mysql_queryf("SELECT useragents.engine as engine, useragents.name as name, clients.os as os, DATE_FORMAT(clients.created, '%Y-%m-%dT%H:%i:%sZ') as since FROM users, clients, useragents WHERE clients.useragent_id=useragents.id AND DATE_ADD(clients.updated, INTERVAL 1 minute) > NOW() AND clients.user_id=users.id AND users.name=%s ORDER BY useragents.engine, useragents.name;", $search_user);
+	$result = mysql_queryf(
+"SELECT
+	useragents.engine as engine,
+	useragents.name as name,
+	clients.os as os,
+	clients.created as since
+FROM
+	users, clients, useragents
+WHERE
+	clients.useragent_id=useragents.id
+	AND DATE_ADD(clients.updated, INTERVAL 1 minute) > NOW()
+	AND clients.user_id=1
+	AND users.name=%s
+ORDER BY
+	useragents.engine,
+	useragents.name;
+", $search_user );
 
-	if ( mysql_num_rows($result) > 0 ) {
+	if ( mysql_num_rows( $result ) > 0 ) {
 
-	echo "<h3>Active Clients:</h3><ul class='clients'>";
+		echo '<h3>Active Clients:</h3><ul class="clients">';
 
-	while ( $row = mysql_fetch_array($result) ) {
-		$engine = $row[0];
-		$browser_name = $row[1];
-		$name = $row[2];
-		$since = $row[3];
+		while ( $row = mysql_fetch_array($result) ) {
+			$engine = $row[0];
+			$browser_name = $row[1];
+			$name = $row[2];
+			$since = $row[3];
 
-		if ( $name == "xp" ) {
-			$name = "Windows XP";
-		} elseif ( $name == "vista" ) {
-			$name = "Windows Vista";
-		} elseif ( $name == "win7" ) {
-			$name = "Windows 7";
-		} elseif ( $name == "2000" ) {
-			$name = "Windows 2000";
-		} elseif ( $name == "2003" ) {
-			$name = "Windows 2003";
-		} elseif ( $name == "osx10.4" ) {
-			$name = "OS X 10.4";
-		} elseif ( $name == "osx10.5" ) {
-			$name = "OS X 10.5";
-		} elseif ( $name == "osx10.6" ) {
-			$name = "OS X 10.6";
-		} elseif ( $name == "osx" ) {
-			$name = "OS X";
-		} elseif ( $name == "linux" ) {
-			$name = "Linux";
+			if ( $name == "xp" ) {
+				$name = "Windows XP";
+			} elseif ( $name == "vista" ) {
+				$name = "Windows Vista";
+			} elseif ( $name == "win7" ) {
+				$name = "Windows 7";
+			} elseif ( $name == "2000" ) {
+				$name = "Windows 2000";
+			} elseif ( $name == "2003" ) {
+				$name = "Windows 2003";
+			} elseif ( $name == "osx10.4" ) {
+				$name = "OS X 10.4";
+			} elseif ( $name == "osx10.5" ) {
+				$name = "OS X 10.5";
+			} elseif ( $name == "osx10.6" ) {
+				$name = "OS X 10.6";
+			} elseif ( $name == "osx" ) {
+				$name = "OS X";
+			} elseif ( $name == "linux" ) {
+				$name = "Linux";
+			}
+
+			echo "<li><img src='" . swarmpath( "/" ) . "images/$engine.sm.png' class='$engine'/> <strong class='name'>$browser_name $name</strong><br>Connected <span title='$since' class='pretty'>$since</span></li>";
 		}
 
-		echo "<li><img src='" . swarmpath( "/" ) . "images/$engine.sm.png' class='$engine'/> <strong class='name'>$browser_name $name</strong><br>Connected <span title='$since' class='pretty'>$since</span></li>";
-	}
-
-	echo "</ul>";
+		echo "</ul>";
 
 	}
 
@@ -72,117 +88,117 @@
 
 	if ( mysql_num_rows($search_result) > 0 ) {
 
-	echo '<br/><h3>Recent Jobs:</h3><table class="results"><tbody>';
+		echo '<br/><h3>Recent Jobs:</h3><table class="results"><tbody>';
 
-	$output = "";
-	$browsers = array();
-	$addBrowser = true;
-	$last = "";
+		$output = "";
+		$browsers = array();
+		$addBrowser = true;
+		$last = "";
 
-	while ( $row = mysql_fetch_array($search_result) ) {
-		$job_name = $row[0];
-		$job_status = get_status(intval($row[1]));
-		$job_id = $row[2];
+		while ( $row = mysql_fetch_array($search_result) ) {
+			$job_name = $row[0];
+			$job_status = get_status(intval($row[1]));
+			$job_id = $row[2];
 
-		$output .= '<tr><th><a href="' . swarmpath( "job/{$job_id}/" ) . '">' . strip_tags($job_name) . "</a></th>\n";
+			$output .= '<tr><th><a href="' . swarmpath( "job/{$job_id}/" ) . '">' . strip_tags($job_name) . "</a></th>\n";
 
-		$results = array();
-		$states = array();
+			$results = array();
+			$states = array();
 
-	$result = mysql_queryf("SELECT runs.id as run_id, runs.url as run_url, runs.name as run_name, useragents.engine as browser, useragents.name as browsername, useragents.id as useragent_id, run_useragent.status as status FROM run_useragent, runs, useragents WHERE runs.job_id=%u AND run_useragent.run_id=runs.id AND run_useragent.useragent_id=useragents.id ORDER BY run_id, browsername;", $job_id);
+			$result = mysql_queryf("SELECT runs.id as run_id, runs.url as run_url, runs.name as run_name, useragents.engine as browser, useragents.name as browsername, useragents.id as useragent_id, run_useragent.status as status FROM run_useragent, runs, useragents WHERE runs.job_id=%u AND run_useragent.run_id=runs.id AND run_useragent.useragent_id=useragents.id ORDER BY run_id, browsername;", $job_id);
 
-	while ( $row = mysql_fetch_assoc($result) ) {
-		if ( $row["run_id"] != $last ) {
-			if ( $last ) {
-				$addBrowser = false;
-			}
-
-			$useragents = array();
-
-			$runResult = mysql_queryf("SELECT run_client.client_id as client_id, run_client.status as status, run_client.fail as fail, run_client.error as error, run_client.total as total, clients.useragent_id as useragent_id, useragents.name as browser FROM useragents, run_client, clients WHERE run_client.run_id=%u AND run_client.client_id=clients.id AND useragents.id=useragent_id ORDER BY browser;", $row["run_id"]);
-
-			while ( $ua_row = mysql_fetch_assoc($runResult) ) {
-				if ( !$useragents[ $ua_row["useragent_id"] ] ) {
-					$useragents[ $ua_row["useragent_id"] ] = array();
-				}
-
-				array_push( $useragents[ $ua_row["useragent_id"] ], $ua_row );
-			}
-		}
-
-		if ( $addBrowser ) {
-			array_push( $browsers, array(
-				"name" => $row["browsername"],
-				"engine" => $row["browser"],
-				"id" => $row["useragent_id"]
-			) );
-		}
-
-		$last_browser = "";
-
-		if ( $useragents[ $row["useragent_id"] ] ) {
-			foreach ( $useragents[ $row["useragent_id"] ] as $ua ) {
-				$status = get_status2(intval($ua["status"]), intval($ua["fail"]), intval($ua["error"]), intval($ua["total"]));
-				if ( $last_browser != $ua["browser"] ) {
-					$cur = $results[ $ua["useragent_id"] ];
-					$results[ $ua["useragent_id"] ] = $cur + intval($ua["fail"]);
-
-					$cur = $states[ $ua["useragent_id"] ];
-
-					if ( strstr($status, "notdone") || strstr($cur, "notdone") ) {
-						$status = "notstarted notdone";
-					} elseif ( $status == "error" || $cur == "error" ) {
-						$status = "error";
-					} elseif ( $status == "timeout" || $cur == "timeout" ) {
-						$status = "timeout";
-					} elseif ( $status == "fail" || $cur == "fail" ) {
-						$status = "fail";
-					} else {
-						$status = "pass";
+			while ( $row = mysql_fetch_assoc($result) ) {
+				if ( $row["run_id"] != $last ) {
+					if ( $last ) {
+						$addBrowser = false;
 					}
 
-					$states[ $ua["useragent_id"] ] = $status;
+					$useragents = array();
+
+					$runResult = mysql_queryf("SELECT run_client.client_id as client_id, run_client.status as status, run_client.fail as fail, run_client.error as error, run_client.total as total, clients.useragent_id as useragent_id, useragents.name as browser FROM useragents, run_client, clients WHERE run_client.run_id=%u AND run_client.client_id=clients.id AND useragents.id=useragent_id ORDER BY browser;", $row["run_id"]);
+
+					while ( $ua_row = mysql_fetch_assoc($runResult) ) {
+						if ( !$useragents[ $ua_row["useragent_id"] ] ) {
+							$useragents[ $ua_row["useragent_id"] ] = array();
+						}
+
+						array_push( $useragents[ $ua_row["useragent_id"] ], $ua_row );
+					}
 				}
-				$last_browser = $ua["browser"];
+
+				if ( $addBrowser ) {
+					array_push( $browsers, array(
+						"name" => $row["browsername"],
+						"engine" => $row["browser"],
+						"id" => $row["useragent_id"]
+					) );
+				}
+
+				$last_browser = "";
+
+				if ( $useragents[ $row["useragent_id"] ] ) {
+					foreach ( $useragents[ $row["useragent_id"] ] as $ua ) {
+						$status = get_status2(intval($ua["status"]), intval($ua["fail"]), intval($ua["error"]), intval($ua["total"]));
+						if ( $last_browser != $ua["browser"] ) {
+							$cur = $results[ $ua["useragent_id"] ];
+							$results[ $ua["useragent_id"] ] = $cur + intval($ua["fail"]);
+
+							$cur = $states[ $ua["useragent_id"] ];
+
+							if ( strstr($status, "notdone") || strstr($cur, "notdone") ) {
+								$status = "notstarted notdone";
+							} elseif ( $status == "error" || $cur == "error" ) {
+								$status = "error";
+							} elseif ( $status == "timeout" || $cur == "timeout" ) {
+								$status = "timeout";
+							} elseif ( $status == "fail" || $cur == "fail" ) {
+								$status = "fail";
+							} else {
+								$status = "pass";
+							}
+
+							$states[ $ua["useragent_id"] ] = $status;
+						}
+						$last_browser = $ua["browser"];
+					}
+				} else {
+						$cur = $results[ $row["useragent_id"] ];
+						$results[ $row["useragent_id"] ] = $cur + 0;
+						$states[ $row["useragent_id"] ] = "notstarted notdone";
+				}
+
+				$last = $row["run_id"];
 			}
-		} else {
-				$cur = $results[ $row["useragent_id"] ];
-				$results[ $row["useragent_id"] ] = $cur + 0;
-				$states[ $row["useragent_id"] ] = "notstarted notdone";
+
+
+			foreach ( $results as $key => $fail ) {
+				$output .= '<td class="' . $states[$key] . '"></td>';
+			}
+
+			$output .= "</tr>\n";
+
 		}
 
-		$last = $row["run_id"];
-	}
-
-
-	foreach ( $results as $key => $fail ) {
-		$output .= '<td class="' . $states[$key] . '"></td>';
-	}
-
-	$output .= "</tr>\n";
-
-	}
-
-	if ( $last ) {
-		$header = "<tr><th></th>\n";
-		$last_browser = array();
-		foreach ( $browsers as $browser ) {
-			if ( $last_browser["id"] != $browser["id"] ) {
-				$header .= '<th><div class="browser">' .
-					'<img src="' . swarmpath( 'images/' ) . $browser["engine"] .
-					'.sm.png" class="browser-icon ' . $browser["engine"] .
-					'" alt="' . $browser["name"] .
-					'" title="' . $browser["name"] .
-					'"/><span class="browser-name">' .
-					preg_replace('/\w+ /', "", $browser["name"]) . ', ' .
-					'</span></div></th>';
+		if ( $last ) {
+			$header = "<tr><th></th>\n";
+			$last_browser = array();
+			foreach ( $browsers as $browser ) {
+				if ( $last_browser["id"] != $browser["id"] ) {
+					$header .= '<th><div class="browser">' .
+						'<img src="' . swarmpath( 'images/' ) . $browser["engine"] .
+						'.sm.png" class="browser-icon ' . $browser["engine"] .
+						'" alt="' . $browser["name"] .
+						'" title="' . $browser["name"] .
+						'"/><span class="browser-name">' .
+						preg_replace('/\w+ /', "", $browser["name"]) . ', ' .
+						'</span></div></th>';
+				}
+				$last_browser = $browser;
 			}
-			$last_browser = $browser;
+			$header .= "</tr>\n";
+			$output = $header . $output;
 		}
-		$header .= "</tr>\n";
-		$output = $header . $output;
-	}
 
-	echo "$output</tr>\n</tbody>\n</table>";
+		echo "$output</tr>\n</tbody>\n</table>";
 
 	}

inc/init-usersession.php
@@ -7,6 +7,7 @@
 	if ( $username ) {
 		$_SESSION["username"] = $username;
 	}
+
 	# We need a username to set up an account
 	if ( !$username ) {
 		# TODO: Improve error message quality.

logic/tinder.php
@@ -1,6 +1,6 @@
 <?php
 
-	$search_user = preg_replace("/[^a-zA-Z0-9_ -]/", "", $_GET["user"]);
+	$search_user = preg_replace( "/[^a-zA-Z0-9_ -]/", "", $swarmRequest->getVal( "user" ) );
 
 	$title = "$search_user";
 	$scripts =