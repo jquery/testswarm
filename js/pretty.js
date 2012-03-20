60f597df490d26b0bcef64338d6f6086382d5b66
Krinkle <krinklemail@gmail.com>
2012-03-21 00:04:44 +0100

    Fix bug in ejohn/prettyDate
    - prettyDate:
     * Support for timezone difference between browser and the string outputted from the server (very likely, as we know from the reports of issue #1)
       Actually very easy to do. The ISO date given ends with a timezone indication.
       I don't know why this was stripped (.replace("[TZ]", "")), but if this isn't
       stripped then the time zone is preserved and interpreted correctly.
    
    - TODO: Push upstream, couldn't find a repo for it, patching locally for now

--- js/pretty.js
+++ js/pretty.js
@@ -7,7 +7,7 @@
 // Takes an ISO time and returns a string representing how
 // long ago the date represents.
 function prettyDate(time) {
-	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+	var date = new Date((time || "")),
 		diff = (((new Date()).getTime() - date.getTime()) / 1000),
 		day_diff = Math.floor(diff / 86400);
 
