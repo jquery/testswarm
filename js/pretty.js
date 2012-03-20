b7790ac21757c60a1b146edd18717ae6df3b6c47
Krinkle <krinklemail@gmail.com>
2012-03-21 00:15:28 +0100

    [prettyDate] clean up
    - fallback to "" is pointless, because "" will return "Invalid Date" or "1970-01-01 00:00:00" depending on the browser. Either way pointless, so return early instead
    
    - add/update documentation
    
    - JSHint, Coding Style

--- js/pretty.js
+++ js/pretty.js
@@ -1,32 +1,46 @@
-/*
+/**
  * JavaScript Pretty Date
- * Copyright (c) 2011 John Resig (ejohn.org)
- * Licensed under the MIT and GPL licenses.
+ *
+ * @author John Resig (ejohn.org), 2008-2011
+ * @author Timo Tijhof, 2012
+ * @license Licensed under the MIT and GPL licenses.
  */
 
-// Takes an ISO time and returns a string representing how
-// long ago the date represents.
+/**
+ * Take a timestamp and turn it into a relative time representation
+ *
+ * @param time String ISO formatted timestamp (YYYY-MM-DDTHH:II:SSZ, where T and Z are literal)
+ * @return String|undefined Relative time in English or undefined if too long ago
+ */
 function prettyDate(time) {
-	var date = new Date(time || ""),
-		diff = ((new Date().getTime() - date.getTime()) / 1000),
-		day_diff = Math.floor(diff / 86400);
+	var date, diff, day_diff;
 
-	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+	if (!time) {
 		return;
+	}
 
-	return day_diff == 0 && (
-			diff < 60 && "just now" ||
+	date = new Date(time);
+	diff = ((new Date().getTime() - date.getTime()) / 1000);
+	day_diff = Math.floor(diff / 86400);
+
+	if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31) {
+		return;
+	}
+
+	return day_diff === 0 && (
+		diff < 60 && "just now" ||
 			diff < 120 && "1 minute ago" ||
-			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-			diff < 7200 && "1 hour ago" ||
-			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-		day_diff == 1 && "Yesterday" ||
+				diff < 3600 && Math.floor(diff / 60) + " minutes ago" ||
+					diff < 7200 && "1 hour ago" ||
+						diff < 86400 && Math.floor(diff / 3600) + " hours ago"
+	) ||
+		day_diff === 1 && "Yesterday" ||
 		day_diff < 7 && day_diff + " days ago" ||
-		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+		day_diff < 31 && Math.ceil(day_diff / 7) + " weeks ago";
 }
 
 // If jQuery is included in the page, adds a jQuery plugin to handle it as well
-if ( typeof jQuery !== "undefined" ) {
+if (typeof jQuery !== "undefined") {
 	jQuery.fn.prettyDate = function () {
 		return this.each(function () {
 			var date = prettyDate(this.title);
