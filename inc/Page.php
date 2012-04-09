jquery/testswarm

commit a15bece58b99255b1f234484aab10ccc682a5ebc

(issue #150) Keep track of software version inside the software
- Although we've been talking about 0.1, 0.2 and 0.3; so far no mention of it
  inside the software. Adding a version.ini file and using it in InfoAction
  and <meta name="generator">

- Also added a primitive git HEAD retriever and cache for it, so that those
  running off master still have meaning full data there (instead of ambiguous
  "0.3.0alpha".

- Follows-up: a52621d5ded4621dc442006d43dc91423807c59e
 * Committed version.ini too soon there

inc/Page.php
@@ -360,6 +360,9 @@ foreach ( $projects as $project ) {
 		// static refers to the current class (FoobarPage)
 		$page = new static();
 		$page->context = $context;
+
+		$page->metaTags[] = array( "name" => "generator", "content" => $context->getConf()->version->testswarm );
+
 		return $page;
 	}
 

inc/actions/InfoAction.php
@@ -13,6 +13,7 @@ class InfoAction extends Action {
 		$conf = $this->getContext()->getConf();
 
 		$info = array(
+			"version" => $conf->version,
 			"conf" => array(
 				"general" => $conf->general,
 				"web" => $conf->web,

inc/init.php
@@ -53,6 +53,11 @@ if ( !file_exists( "$swarmInstallDir/testswarm.ini" ) ) {
 }
 
 $swarmConfig = array(
+	// Not included in testswarm-sample.ini, not meant to be overridden,
+	// although users could ammend it to include something like "-foobarPatched".
+	"version" => array(
+		"testswarm" => "", // populated later
+	),
 	"general" => array(
 		"timezone" => "UTC",
 	),
@@ -119,6 +124,8 @@ $swarmConfig["web"]["ajax_update_interval"] = intval( $swarmConfig["web"]["ajax_
 
 // Caching dir
 $swarmConfig["storage"]["cacheDir"] = str_replace( "$1", $swarmInstallDir, $swarmConfig["storage"]["cacheDir"] );
+
+$swarmConfig["version"]["testswarm"] = swarmGetVersion( $swarmConfig["storage"]["cacheDir"] . "/version_testswarm.cache" );
 
 // Refresh control
 // (for documentation see testswarm.ini)

inc/utilities.php
@@ -220,14 +220,14 @@
 		if ( is_null( $contextpath ) ) {
 			// Add trailing slash if it's missing
 			$path = $swarmContext->getConf()->web->contextpath;
-			if ( substr( $path, -1 ) !== '/' ) {
+			if ( substr( $path, -1 ) !== "/" ) {
 				$path = "$path/";
 			}
 
 			// Make sure path starts absolute.
 			// Either with protocol https?://domain, relative-protocol //domain
 			// or starting at domain root with a slash.
-			if ( substr( $path, 0, 6 ) !== 'http:/' && substr( $path, 0, 6 ) !== 'https:' && $path[0] !== '/' ) {
+			if ( substr( $path, 0, 6 ) !== "http:/" && substr( $path, 0, 6 ) !== "https:" && $path[0] !== "/" ) {
 				$path = "/$path";
 			}
 
@@ -238,9 +238,76 @@
 		// Just in case, strip the leading slash
 		// from the requested path (check length, becuase may be an empty string,
 		// avoid PHP E_NOTICE for undefined [0], which could JSON output to be interrupted)
-		if ( strlen( $rel ) > 0 && $rel[0] === '/' ) {
+		if ( strlen( $rel ) > 0 && $rel[0] === "/" ) {
 			$rel = substr($rel, 1);
 		}
 
 		return $path . $rel;
+	}
+
+	/**
+	 * Get the current TestSwarm version (cached for 5 minutes).
+	 * It is a cheap operation but we want to avoid repeatative lookups.
+	 *
+	 * @author Timo Tijhof, 2012
+	 * @since 0.3.0
+	 *
+	 * @param $versionCacheFile string: Path to cache file,
+	 * parent directory must be writable by the script!
+	 * @return string: e.g. "0.3.0" (e.g. for installs from a tar or zip),
+	 * or something like "0.3.0-alpha (hash)" for installs on a live Git repo.
+	 */
+	function swarmGetVersion( $versionCacheFile ) {
+		static $versionCached;
+		if ( $versionCached !== null ) {
+			return $versionCached;
+		}
+
+		// Cache 5 minutes
+		$versionCacheFileUpdated = filemtime( $versionCacheFile );
+		if ( $versionCacheFileUpdated < strftime( '5 minutes ago' ) ) {
+			unlink( $versionCacheFile );
+		}
+
+		if ( !is_readable( $versionCacheFile ) ) {
+			global $swarmInstallDir;
+
+			$baseVersionFile = "$swarmInstallDir/config/version.ini";
+			if ( !is_readable( $baseVersionFile ) ) {
+				throw new SwarmException( "version.ini is missing or unreadable." );
+			}
+			$version = trim( file_get_contents( $baseVersionFile ) );
+
+			// If this is a git repository, get a hold of the HEAD SHA1 hash as well,
+			// and append it to the version.
+			$gitHeadFile = "$swarmInstallDir/.git/HEAD";
+			if ( is_readable( $gitHeadFile ) ) {
+				$gitHead = file_get_contents( $gitHeadFile );
+				if ( preg_match( "/ref: (.*)/", $gitHead, $matches ) ) {
+					$gitHead = rtrim( $matches[1] );
+				} else {
+					$gitHead = trim( $gitHead );
+				}
+
+				$gitRefFile = "$swarmInstallDir/.git/$gitHead";
+				if ( is_readable( $gitRefFile ) ) {
+					$gitSHA1 = rtrim( file_get_contents( $gitRefFile ) );
+				} else {
+					// If such refs file doesn't exist, maybe HEAD is detached,
+					// in which case ./.git/HEAD should contain the actual SHA1 already.
+					$gitSHA1 = $gitHead;
+				}
+
+				$version .= " (" . substr( $gitSHA1, 0, 8 ) . ")";
+				$isWritten = (bool)file_put_contents( $versionCacheFile, $version );
+				if ( !$isWritten ) {
+					throw new SwarmException( "Version cache must be writable." );
+				}
+				$versionCached = $version;
+				return $versionCached;
+			}
+		} else {
+			$versionCached = trim( file_get_contents( $versionCacheFile ) );
+			return $versionCached;
+		}
 	}

config/version.ini
@@ -1,1 +1,1 @@
-version=0.3.0-alpha
+0.3.0-alpha

commit bcc70aed9dccd2687f06280a48cc1270dd791f46

uses context in swarmGetVersion() and handle cache dir error in init.php
- Fixes:
 * (issue #151): Cache dir error handling

- Follows-up a15bece58b99255b1f234484aab10ccc682a5ebc which introduced the
  possibility for init.php to throw an exception which it shouldn't.
  (not directly, but indirectly by calling swarmGetVersion).

- Also removed the static variable in swarmGetVersion, this was somewhat
  unneeded since it is already cached and also possibly wrong since it
  took the cache file as parameter without taking that into account of the
  static cache. We don't need to worry about this extra layer of caching
  since this function is rarely or never called more than once per request.

  Improved comments and fixed logic error in Page::httpStatusHeader

inc/Page.php
@@ -344,10 +344,9 @@ foreach ( $projects as $project ) {
 
 	final public static function httpStatusHeader( $code ) {
 		$message = self::getHttpStatusMsg( $code );
-		if ( !$message ) {
-			throw new SwarmError( "Unknown http code." );
+		if ( $message ) {
+			header( $_SERVER["SERVER_PROTOCOL"] . " $code $message", true, $code );
 		}
-		header( $_SERVER["SERVER_PROTOCOL"] . " $code $message", true, $code );
 	}
 
 	final public static function getPageClassByName( $pageName ) {
@@ -361,7 +360,7 @@ foreach ( $projects as $project ) {
 		$page = new static();
 		$page->context = $context;
 
-		$page->metaTags[] = array( "name" => "generator", "content" => $context->getConf()->version->testswarm );
+		$page->metaTags[] = array( "name" => "generator", "content" => swarmGetVersion( $context ) );
 
 		return $page;
 	}

inc/actions/InfoAction.php
@@ -13,7 +13,7 @@ class InfoAction extends Action {
 		$conf = $this->getContext()->getConf();
 
 		$info = array(
-			"version" => $conf->version,
+			"version" => swarmGetVersion( $this->getContext() ),
 			"conf" => array(
 				"general" => $conf->general,
 				"web" => $conf->web,

inc/init.php
@@ -4,6 +4,15 @@
  * All web requests have to go through here,
  * and do so as early as possible.
  *
+ * This file should NEVER throw exceptions as they can't be
+ * caught in a nice way until either Api or Page is reached.
+ * Instead critical issues with the environment should just
+ * result in a straight death sentence of the proces and
+ * send out a plain text message. Currently:
+ * - phpversion support
+ * - testswarm.ini existance
+ * - cache dir existance and writability
+ *
  * @author Timo Tijhof, 2012
  * @since 0.3.0
  * @package TestSwarm
@@ -21,7 +30,7 @@ if ( !defined( 'TESTSWARM' ) ) {
 
 // Minimum PHP version
 if ( !function_exists( 'version_compare' ) || version_compare( phpversion(), '5.2.3' ) < 0 ) {
-	echo "TestSwarm requires at least PHP 5.2.3\n";
+	echo "<b>TestSwarm Fatal:</b> TestSwarm requires at least PHP 5.2.3.\n";
 	exit;
 }
 
@@ -48,16 +57,11 @@ $swarmInstallDir = dirname( __DIR__ );
 
 // Verify that the testswarm.ini file exists
 if ( !file_exists( "$swarmInstallDir/testswarm.ini" ) ) {
-	echo "testswarm.ini missing!\n";
+	echo "<b>TestSwarm Fatal:</b> testswarm.ini missing!\n";
 	exit;
 }
 
 $swarmConfig = array(
-	// Not included in testswarm-sample.ini, not meant to be overridden,
-	// although users could ammend it to include something like "-foobarPatched".
-	"version" => array(
-		"testswarm" => "", // populated later
-	),
 	"general" => array(
 		"timezone" => "UTC",
 	),
@@ -122,10 +126,13 @@ $swarmConfig["client"]["refresh_control"] = intval( $swarmConfig["client"]["refr
 
 $swarmConfig["web"]["ajax_update_interval"] = intval( $swarmConfig["web"]["ajax_update_interval"] );
 
-// Caching dir
 $swarmConfig["storage"]["cacheDir"] = str_replace( "$1", $swarmInstallDir, $swarmConfig["storage"]["cacheDir"] );
 
-$swarmConfig["version"]["testswarm"] = swarmGetVersion( $swarmConfig["storage"]["cacheDir"] . "/version_testswarm.cache" );
+// Caching directory must exist and be writable
+if ( !is_dir( $swarmConfig["storage"]["cacheDir"] ) || !is_writable( $swarmConfig["storage"]["cacheDir"] ) ) {
+	echo "<b>TestSwarm Fatal</b>: Caching directory must exist and be writable by the script!\n";
+	exit;
+}
 
 // Refresh control
 // (for documentation see testswarm.ini)

inc/utilities.php
@@ -252,18 +252,14 @@
 	 * @author Timo Tijhof, 2012
 	 * @since 0.3.0
 	 *
-	 * @param $versionCacheFile string: Path to cache file,
-	 * parent directory must be writable by the script!
+	 * @param $context TestSwarmContext
 	 * @return string: e.g. "0.3.0" (e.g. for installs from a tar or zip),
 	 * or something like "0.3.0-alpha (hash)" for installs on a live Git repo.
 	 */
-	function swarmGetVersion( $versionCacheFile ) {
-		static $versionCached;
-		if ( $versionCached !== null ) {
-			return $versionCached;
-		}
+	function swarmGetVersion( TestSwarmContext $context ) {
+		$versionCacheFile = $context->getConf()->storage->cacheDir . "/version_testswarm.cache";
 
-		// Cache 5 minutes
+		// Clear cache older than 5 minutes
 		if ( is_readable( $versionCacheFile ) ) {
 			$versionCacheFileUpdated = filemtime( $versionCacheFile );
 			if ( $versionCacheFileUpdated < strftime( '5 minutes ago' ) ) {
@@ -271,6 +267,7 @@
 			}
 		}
 
+		// If cache has just been cleared or didn't exist yet, (re)populate it.
 		if ( !is_readable( $versionCacheFile ) ) {
 			global $swarmInstallDir;
 
@@ -308,6 +305,8 @@
 				$versionCached = $version;
 				return $versionCached;
 			}
+
+		// Get from cache
 		} else {
 			$versionCached = trim( file_get_contents( $versionCacheFile ) );
 			return $versionCached;