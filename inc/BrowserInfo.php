jquery/testswarm

commit fd24b5e8cce17331c3e3dca3060a8231a5818a9f

Document and improve database columns and indices.
- Walked through query logs and "EXPLAIN" dumps and acked/grepped
  through the source. Except for a few 'known to be slow' queries
  left, every major query is now optimized to use an index or
  I created an index for it.

  Although my local mini swarm is not a significant test base,
  I noticed that most select queries speeded up from 8 ~ 20ms to 7 ~ 16ms.

- Added documentation to the columns.

- Removed value limits where it didn't make sense (such as int(11) on an
  auto-incrementing column. Also no way they are going (or are supposed to go)
  negative, so made them unsigned as well.

  Changed pattern:
  > foo_id int(11) NOT NULL auto_increment .. .. .. PRIMARY KEY  (`foo_id`)
  into:
  > foo_od int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT

- Using integer values for int column defaults (0 instead of '0').

- Reorders table creation statements in order of the foreign key restrictions.

- Added missing "error" parameter in run.js for the timeout saverun
  submission. The server side indirectly defaults to 0, but SaverunAction does
  is documented to require the parameter so explicitly sending it now
  (just like inject.js has been doing already).

- Use `id = %u` instead of `id = %s` for int columns
  (most queries did this already, except for few in Client.php and
  RunresultsPage.php).

- Remove unneeded join to `clients` in SaverunAction for queries to
  `run_client` and `run_useragent`.
  No need to filter by ua ID as the client_id is unique and already verified
  to belong to the requesting user-agent at this point.

inc/actions/JobAction.php
@@ -32,7 +32,7 @@ class JobAction extends Action {
 			FROM
 				jobs, users
 			WHERE jobs.id = %u
-			AND   users.id=jobs.user_id;",
+			AND   users.id = jobs.user_id;",
 			$jobID
 		));
 
@@ -229,7 +229,7 @@ class JobAction extends Action {
 				return "timedout";
 			}
 			// A total of 0 tests ran is also considered an error
-			if ( $clientRun->error > 0 || $clientRun->total == 1 ) {
+			if ( $clientRun->error > 0 || $clientRun->total == 0 ) {
 				return "error";
 			}
 			// Passed or failed

inc/actions/SaverunAction.php
@@ -78,15 +78,12 @@ class SaverunAction extends Action {
 				$rows = $db->getRows(str_queryf(
 					"SELECT client_id
 					FROM
-						run_client, clients
+						run_client
 					WHERE run_id = %u
 					AND   client_id != %u
-					AND   (total <= 0 OR error > 0 OR fail > 0)
-					AND   clients.id = client_id
-					AND   clients.useragent_id = %s;",
+					AND   (total <= 0 OR error > 0 OR fail > 0);",
 					$runID,
-					$clientID,
-					$browserInfo->getSwarmUaID()
+					$clientID
 				));
 
 				if ( $rows ) {
@@ -126,26 +123,25 @@ class SaverunAction extends Action {
 						"SELECT
 							client_id
 						FROM
-							run_client, clients
+							run_client
 						WHERE run_id = %u
 						AND   client_id != %u
-						AND   total <= 0
-						AND   clients.id = client_id
-						AND   clients.useragent_id = %s;",
+						AND   total <= 0;",
 						$runID,
-						$clientID,
-						$browserInfo->getSwarmUaID()
+						$clientID
 					));
 
-					foreach ( $rows as $row ) {
-						$db->query(str_queryf(
-							"DELETE
-							FROM run_client
-							WHERE run_id = %u
-							AND   client_id = %u;",
-							$runID,
-							$row->client_id
-						));
+					if ( $rows ) {
+						foreach ( $rows as $row ) {
+							$db->query(str_queryf(
+								"DELETE
+								FROM run_client
+								WHERE run_id = %u
+								AND   client_id = %u;",
+								$runID,
+								$row->client_id
+							));
+						}
 					}
 				}
 

inc/actions/WiperunAction.php
@@ -55,7 +55,7 @@ class WiperunAction extends Action {
 				job_id
 			FROM
 				runs
-			WHERE runs.id=%u;",
+			WHERE runs.id = %u;",
 			$runID
 		));
 		if ( intval( $runJobID ) !== $jobID ) {
@@ -81,10 +81,8 @@ class WiperunAction extends Action {
 			FROM
 				run_client, clients
 			WHERE run_id = %u
-			AND   clients.id = client_id
-			AND   clients.useragent_id = %s;",
-			$runID,
-			$useragentID
+			AND   clients.id = client_id;",
+			$runID
 		));
 		$db->query(str_queryf(
 			"UPDATE

config/testswarm.sql
@@ -1,122 +1,230 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+-- Insertions handled by the SignupAction class.
+--
+
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+  -- User name used for display and login prodecure.
+  `name` varchar(255) binary NOT NULL default '',
+
+  -- User account details last modified (YYYYMMDDHHMMSS timestamp)
+  -- Right now this is only set during creation, no update statement
+  -- exists yet in TestSwarm, but for consistency with other tables
+  -- it has been created for future use.
+  `updated` binary(14) NOT NULL,
+
+  -- User account originally created (YYYYMMDDHHMMSS timestamp).
+  `created` binary(14) NOT NULL,
+
+  -- Password hash salt (SHA1).
+  `seed` binary(40) NOT NULL default '',
+
+  -- Password hash (SHA1).
+  `password` binary(40) NOT NULL default '',
+
+  -- Authentication token to use in situations where sending passwords
+  -- is not an option. Used for the "addjob" API.
+  `auth` binary(40) NOT NULL default ''
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE UNIQUE INDEX idx_users_name ON users (name);
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `clients`
+-- Insertions and updates handled by the Client class.
 --
 
 CREATE TABLE `clients` (
-  `id` int(11) NOT NULL auto_increment,
-  `user_id` int(11) NOT NULL default '0',
-  `useragent_id` varchar(40) NOT NULL,
+  `id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+  -- Key to users.id field.
+  `user_id` int unsigned NOT NULL,
+
+  -- Key to useragents.ini section.
+  `useragent_id` varchar(255) NOT NULL default '',
+
+  -- Raw User-Agent string.
   `useragent` tinytext NOT NULL,
-  `ip` varchar(15) NOT NULL default '',
+
+  -- Raw IP string as extractred by WebRequest::getIP
+  `ip` varbinary(40) NOT NULL default '',
+
+  -- YYYYMMDDHHMMSS timestamp.
   `updated` binary(14) NOT NULL,
-  `created` binary(14) NOT NULL,
-  PRIMARY KEY  (`id`)
+
+  -- YYYYMMDDHHMMSS timestamp.
+  `created` binary(14) NOT NULL
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Used on the HomePage and SwarmstateAction.
+CREATE INDEX idx_clients_useragent_updated ON clients (useragent_id, updated);
+
+-- Used for listing in UserAction and ScoresAction.
+-- Also used to verify BrowserInfo/Client match.
+CREATE INDEX idx_clients_user_useragent_updated ON clients (user_id, useragent_id, updated);
+
+-- Foreign key constrains
+ALTER TABLE clients
+	ADD CONSTRAINT fk_clients_user_id FOREIGN KEY (user_id) REFERENCES users (id);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `jobs`
+-- Insertions handled by the AddjobAction class.
 --
 
 CREATE TABLE `jobs` (
-  `id` int(11) NOT NULL auto_increment,
-  `user_id` int(11) NOT NULL default '0',
-  `name` varchar(255) NOT NULL default '',
-  `created` binary(14) NOT NULL,
-  PRIMARY KEY  (`id`)
+  `id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+  -- Key to users.id field.
+  `user_id` int unsigned NOT NULL,
+
+  -- Job name (can contain HTML)
+  `name` varchar(255) binary NOT NULL default '',
+
+  -- YYYYMMDDHHMMSS timestamp.
+  `created` binary(14) NOT NULL
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- Used in UserAction.
+CREATE INDEX idx_jobs_user ON clients (user_id, created);
 
---
--- Table structure for table `run_client`
---
-
-CREATE TABLE `run_client` (
-  `run_id` int(11) NOT NULL default '0',
-  `client_id` int(11) NOT NULL default '0',
-  `status` tinyint(4) NOT NULL default '0',
-  `fail` int(11) NOT NULL default '0',
-  `error` int(11) NOT NULL default '0',
-  `total` int(11) NOT NULL default '0',
-  `results` text NOT NULL,
-  `updated` binary(14) NOT NULL,
-  `created` binary(14) NOT NULL,
-  PRIMARY KEY  (`run_id`,`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `run_useragent`
---
-
-CREATE TABLE `run_useragent` (
-  `run_id` int(11) NOT NULL default '0',
-  `useragent_id` varchar(40) NOT NULL,
-  `runs` int(11) NOT NULL default '0',
-  `max` int(11) NOT NULL default '1',
-  `completed` int(11) NOT NULL default '0',
-  `status` tinyint(4) NOT NULL default '0',
-  `updated` binary(14) NOT NULL,
-  `created` binary(14) NOT NULL,
-  PRIMARY KEY  (`run_id`,`useragent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Foreign key constrains
+ALTER TABLE jobs
+	ADD CONSTRAINT fk_jobs_user_id FOREIGN KEY (user_id) REFERENCES users (id);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `runs`
+-- Insertions handled by the AddjobAction class.
 --
 
 CREATE TABLE `runs` (
-  `id` int(11) NOT NULL auto_increment,
-  `job_id` int(11) NOT NULL default '0',
-  `name` varchar(255) NOT NULL default '',
+  `id` int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+  -- Key to jobs.id field.
+  `job_id` int unsigned NOT NULL,
+
+  -- Run name
+  `name` varchar(255) binary NOT NULL default '',
+
+  -- Run url
   `url` tinytext NOT NULL,
-  `created` binary(14) NOT NULL,
-  PRIMARY KEY  (`id`)
+
+  -- YYYYMMDDHHMMSS timestamp.
+  `created` binary(14) NOT NULL
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Used in JobAction.
+CREATE INDEX idx_runs_jobid ON runs (job_id);
+
+ALTER TABLE runs
+	ADD CONSTRAINT fk_runs_job_id FOREIGN KEY (job_id) REFERENCES jobs (id);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `run_useragent`
+-- Insertions handled by the AddjobAction class. Updates by SaverunAction,
+-- WipejobAction, and WiperunAction.
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) NOT NULL,
+CREATE TABLE `run_useragent` (
+
+  -- Key to runs.id field.
+  `run_id` int unsigned NOT NULL default 0,
+
+  -- Key to useragents.ini section.
+  `useragent_id` varchar(255) NOT NULL default '',
+
+  -- Similar to `completed`, except that this one is used by queries
+  -- to figure out whether this run should be (re-)ran. If a run completes
+  -- passing 100%, this will be set to `max` immediately.
+  `runs` int unsigned NOT NULL default 0,
+
+  -- Addjob runMax
+  `max` int unsigned NOT NULL default 1,
+
+  -- Number of times this run has run to completion for this user agent.
+  -- In most cases this will end up being set to 1 once and then never
+  -- touched again. If a client completes a run with one or more failed
+  -- unit tests, another client will get the same run to phase out / reduce the
+  -- risk of false negatives due to coindicing browser/connectivity issues,
+  -- until `max` is reached.
+  `completed` int unsigned NOT NULL default 0,
+
+  -- Run status (0 = new, 1 = progress, 2 = complete).
+  -- See also JobAction::resolveStatusID.
+  `status` tinyint(4) NOT NULL default 0,
+
+  -- YYYYMMDDHHMMSS timestamp.
   `updated` binary(14) NOT NULL,
-  `created` binary(14) NOT NULL,
-  `seed` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL,
-  `auth` varchar(40) NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `name` (`name`)
+
+  -- YYYYMMDDHHMMSS timestamp.
+  `created` binary(14) NOT NULL
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Indexes and Foreign Keys
-alter table clients
-	add index idx_clients_user_id (user_id),
-	add constraint fk_clients_user_id foreign key (user_id) references users (id);
-alter table clients
-	add index idx_clients_useragent_id (useragent_id);
-alter table jobs
-	add index idx_jobs_user_id (user_id),
-	add constraint fk_jobs_user_id foreign key (user_id) references users (id);
-alter table run_client
-	add index idx_run_client_run_id (run_id),
-	add constraint fk_run_client_run_id foreign key (run_id) references runs (id);
-alter table run_client
-	add index idx_run_client_client_id (client_id),
-	add constraint fk_run_client_client_id foreign key (client_id) references clients (id);
-alter table run_useragent
-	add index idx_run_useragent_run_id (run_id),
-	add constraint fk_run_useragent_run_id foreign key (run_id) references runs (id);
-alter table run_useragent
-	add index idx_run_useragent_useragent_id (useragent_id);
-alter table runs
-	add index idx_runs_job_id (job_id),
-	add constraint fk_runs_job_id foreign key (job_id) references jobs (id);
+CREATE UNIQUE INDEX run_useragent_run_useragent ON run_useragent (run_id, useragent_id);
+CREATE INDEX run_useragent_useragent ON run_useragent (useragent_id);
+
+ALTER TABLE run_useragent
+	ADD CONSTRAINT fk_run_useragent_run_id FOREIGN KEY (run_id) REFERENCES runs (id);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `run_client`
+-- Insertions handled by the GerunAction class. Updates by SaverunAction.
+-- Removal from CleanupAction, WipejobAction, and WiperunAction.
+
+CREATE TABLE `run_client` (
+
+  -- Key to runs.id field.
+  `run_id` int unsigned NOT NULL,
+
+  -- Key to clients.id field.
+  `client_id` int unsigned NOT NULL,
+
+  -- Run status (0 = new, 1 = progress, 2 = complete).
+  -- See also JobAction::resolveStatusID and JobAction::getStatusFromClientRunRow.
+  `status` tinyint(4) NOT NULL default 0,
+
+  -- Number of failed tests. -1 indicates a time out.
+  `fail` int NOT NULL default 0,
+
+  -- Number of errors.
+  `error` int unsigned NOT NULL default 0,
+
+  -- Total number of tests ran. -1 indicates a time out.
+  `total` int NOT NULL default 0,
+
+  -- HTML snapshot of the test results page.
+  `results` text NOT NULL,
+
+  -- YYYYMMDDHHMMSS timestamp.
+  `updated` binary(14) NOT NULL,
+
+  -- YYYYMMDDHHMMSS timestamp.
+  `created` binary(14) NOT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE UNIQUE INDEX run_client_run_client ON run_client (run_id, client_id);
+
+ALTER TABLE run_client
+	ADD CONSTRAINT fk_run_client_run_id FOREIGN KEY (run_id) REFERENCES runs (id);
+ALTER TABLE run_client
+	ADD CONSTRAINT fk_run_client_client_id FOREIGN KEY (client_id) REFERENCES clients (id);

inc/Client.php
@@ -103,13 +103,15 @@ class Client {
 		// If the user doesn't have one, create a new user row for this name
 		if ( !$userRow || !$userRow->id ) {
 			$db->query(str_queryf(
+				// This omits some of the required columns but seems to work regardless.
+				// See also github.com/jquery/testswarm/issues/148 which will fix this.
 				"INSERT INTO users (name, updated, created) VALUES(%s, %s, %s);",
 				$username,
 				swarmdb_dateformat( SWARM_NOW ),
 				swarmdb_dateformat( SWARM_NOW )
 			));
 			$userRow = $db->getRow(str_queryf(
-				"SELECT * FROM users WHERE id = %s LIMIT 1;",
+				"SELECT * FROM users WHERE id = %u LIMIT 1;",
 				$db->getInsertId()
 			));
 		}
@@ -127,7 +129,7 @@ class Client {
 		));
 
 		$this->clientRow = $db->getRow(str_queryf(
-			"SELECT * FROM clients WHERE id = %s LIMIT 1;",
+			"SELECT * FROM clients WHERE id = %u LIMIT 1;",
 			$db->getInsertId()
 		));
 		$this->userRow = $userRow;

inc/actions/GetrunAction.php
@@ -63,8 +63,8 @@ class GetrunAction extends Action {
 					runs.name as run_name
 				FROM
 					runs, jobs
-				WHERE runs.id=%u
-				AND   jobs.id=runs.job_id
+				WHERE runs.id = %u
+				AND   jobs.id = runs.job_id
 				LIMIT 1;",
 				$runID
 			));

inc/pages/RunresultsPage.php
@@ -26,8 +26,8 @@ class RunresultsPage extends Page {
 					results
 				FROM
 					run_client
-				WHERE run_id = %s
-				AND   client_id = %s;",
+				WHERE run_id = %u
+				AND   client_id = %u;",
 				$runID, $clientID
 			));
 

js/run.js
@@ -95,11 +95,12 @@
 			{
 				action: "saverun",
 				fail: "-1",
+				error: "0",
 				total: "-1",
 				results: "Test Timed Out.",
 				run_id: currRunId,
 				client_id: SWARM.client_id
-			}
+			},
 			testTimedout,
 			function ( data ) {
 				if ( data.saverun === "ok" ) {

commit dba00276c05ce0ce0db0c46b23a07b1b1cc3d633

various bugs
- Re-fix issue #95 (user-agent sort order)
 * When we switched to the INI file in most places it was fixed
   as the order of the file is used, but when quering the run_client
   table in JobAction, it is ordered by useragent_id then then
   we have the same thing with IE|10 < IE|6.

   Using natksort() on the resulting array.

- If a test is complete with no errors, but with failures it
  was showing a green background but the failure count inside.

  Caused by a regression in the write, accidentally typed ->error
  where it should be ->fail

- SaverunAction deletes run_client rows of the same run where
  client_id != $clientID and status != passing.

  That looks good, except that it also nukes run_client rows
  of other runs of the same run but for a different useragent_id.
  Adding (back) the useragent_id check.

- WiperunAction was only removing the run_client row of the
  latest known client to run the run.

  This resulted in weird behavior where if a run failed 'max' times,
  triggering wipe run would only delete one and the table cell would
  still be red (but a different run_client shining through), and
  would require 'max' number of times to wipe them all,
  This should instead wipe all runs by user agent with a join
  to clients table.
  (regression in fd24b5e8cce17331c3e3dca3060a8231a5818a9f where
  the user agent filter was incidentally removed from the query).

inc/BrowserInfo.php
@@ -44,6 +44,7 @@ class BrowserInfo {
 			// to an object with boolean values
 			$swarmUaIndex = new stdClass;
 			$rawIndex = parse_ini_file( "$swarmInstallDir/config/useragents.ini", true );
+			natcaseksort( $rawIndex );
 			foreach ( $rawIndex as $uaID => $uaItem ) {
 				if ( is_array( $uaItem ) ) {
 					$uaItem2 = $uaItem;

inc/actions/JobAction.php
@@ -164,6 +164,7 @@ class JobAction extends Action {
 								),
 					);
 				}
+				natcaseksort( $runUaRuns );
 			}
 
 			$respData["runs"][] = array(
@@ -181,6 +182,7 @@ class JobAction extends Action {
 				$respData["userAgents"][$userAgentID] = (array)$swarmUaIndex->$userAgentID;
 			}
 		}
+		natcaseksort( $respData["userAgents"] );
 
 		// Save data
 		$this->setData( $respData );
@@ -233,7 +235,7 @@ class JobAction extends Action {
 				return "error";
 			}
 			// Passed or failed
-			return $clientRun->error > 0 ? "failed" : "passed";
+			return $clientRun->fail > 0 ? "failed" : "passed";
 		} else {
 			throw new SwarmException( "Corrupt useragent run result." );
 		}

inc/actions/SaverunAction.php
@@ -81,17 +81,21 @@ class SaverunAction extends Action {
 		if ( mysql_affected_rows() > 0 ) {
 
 			// If we're 100% passing we don't need any more runs
-			// Clear out old runs that were bad, since we now have a good one
+			// Clear out other entries from other browsers for the same run
+			// that were bad, since we now have a good one.
 			if ( $total > 0 && $fail === 0 && $error === 0 ) {
 				$rows = $db->getRows(str_queryf(
 					"SELECT client_id
 					FROM
-						run_client
+						run_client, clients
 					WHERE run_id = %u
 					AND   client_id != %u
-					AND   (total <= 0 OR error > 0 OR fail > 0);",
+					AND   (total <= 0 OR error > 0 OR fail > 0)
+					AND   clients.id = client_id
+					AND   clients.useragent_id = %s;",
 					$runID,
-					$clientID
+					$clientID,
+					$client->getClientRow()->useragent_id
 				));
 
 				if ( $rows ) {

inc/actions/WiperunAction.php
@@ -79,11 +79,12 @@ class WiperunAction extends Action {
 		$db->query(str_queryf(
 			"DELETE run_client
 			FROM
-				run_client
+				run_client, clients
 			WHERE run_id = %u
-			AND   client_id = %u;",
+			AND   clients.id = client_id
+			AND   clients.useragent_id = %s;",
 			$runID,
-			$clientID
+			$useragentID
 		));
 
 		$db->query(str_queryf(

inc/utilities.php
@@ -162,6 +162,29 @@
 		return $sql_query;
 	}
 
+	if ( !function_exists( 'natksort' ) ) {
+		/**
+		 * PHP has natsort() but no natksort().
+		 *
+		 * @source http://stackoverflow.com/a/1186347/319266
+		 * @seealso php.net/uksort, php.net/natsort, php.net/strnatcmp
+		 */
+		 function natksort( &$array ) {
+			uksort( $array, 'strnatcmp' );
+		}
+	}
+	if ( !function_exists( 'natcaseksort' ) ) {
+		/**
+		 * PHP has natcasesort() but no natcaseksort().
+		 *
+		 * @source http://stackoverflow.com/a/1186347/319266
+		 * @seealso php.net/uksort, php.net/natcasesort, php.net/strnatcasecmp
+		 */
+		 function natcaseksort( &$array ) {
+			uksort( $array, 'strnatcasecmp' );
+		}
+	}
+
 	/**
 	 * Convert a date string into a Unix timestamp.
 	 * Interpreteting the date string in GMT context (instead of the time zone currently