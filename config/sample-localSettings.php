86c137bd58308f3d29d6e928ad25512ddfdcf4cb
Timo Tijhof <krinklemail@gmail.com>
2012-11-20 16:29:43 +0100

    Config: Fix error in sample-localSettings.php
    Messed up locally when debugging something, synchronising the
    sample now with the real one.
    
    Follows-up cd33f570cc98abf900df6ee25fb91d274cf675d2

--- config/sample-localSettings.php
+++ config/sample-localSettings.php
@@ -5,7 +5,8 @@
  *  If something else, it will be silently ignored and logged to error_log.
  */
 $settingsFile = __DIR__ . '/localSettings.json';
-
-if ( is_readable( $settingsFile ) ) {
-	return json_decode( file_get_contents( $settingsFile ) ) or error_log( 'TestSwarm Warning: Unable to parse localSettings.json.' );
+$settings = is_readable( $settingsFile ) ? json_decode( file_get_contents( $settingsFile ) ) : null;
+if ( !$settings ) {
+	error_log( 'TestSwarm Warning: Unable to parse localSettings.json.' );
 }
+return $settings;
