a46a9a620c2ca2c2e71fc5eaa59ce84ac350c717
Timo Tijhof <krinklemail@gmail.com>
2017-08-23 19:25:50 +0100

    RunPage: Move iframes container to be before the history element
    Ref https://github.com/jquery/jquery-migrate/issues/255
    Ref https://github.com/jquery/testswarm/issues/316

--- inc/pages/RunPage.php
+++ inc/pages/RunPage.php
@@ -43,11 +43,11 @@ class RunPage extends Page {
 					. '<p><strong>Status:</strong> <span id="msg"></span></p>'
 				. '</div>'
 			. '</div>'
+			. '<div id="iframes"></div>'
 			. '<div class="well">'
 				. '<h3>History</h3>'
 				. '<ul id="history"></ul>'
-			. '</div>'
-			. '<div id="iframes"></div>';
+			. '</div>';
 
 		return $html;
 	}
